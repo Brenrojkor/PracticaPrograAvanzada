2c8bf4fcd4278070f51d34ba483841f0b9d86f7c
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-10-28 10:27:46 -0600

    Create ConexionController.cs

---
+++ PracticaProgramada2_Grupo2/Controllers/ConexionController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+
+namespace PracticaProgramada2_Grupo2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ConexionController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConexionController(IConfiguration configuration) 
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public ActionResult Conectar() 
+        {
+            string connectionString = _configuration.GetConnectionString("Minombredeconexion");
+
+            try 
+            {
+                using (var conexion = new MySqlConnection(connectionString))
+                {
+                    conexion.Open();
+                    return Ok("Conexión Exitosa. ");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
