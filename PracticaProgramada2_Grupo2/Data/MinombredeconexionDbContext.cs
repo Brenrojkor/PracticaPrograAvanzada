1c41e67eda0ed136a515532cb39bf0ed5f902be6
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-10-27 19:34:39 -0600

    .

--- PracticaProgramada2_Grupo2/Data/MinombredeconexionDbContext.cs
+++ PracticaProgramada2_Grupo2/Data/MinombredeconexionDbContext.cs
@@ -1,11 +1,10 @@
 ﻿using PracticaProgramada2_Grupo2.Controllers;
 using PracticaProgramada2_Grupo2.Models;
-
+using Microsoft.EntityFrameworkCore;
 
 namespace PracticaProgramada2_Grupo2.Data
 {
-    public class MinombredeconexionDbContextcs
+    public class MinombredeconexionDbContext
     {
-
     }
 }
