6e267728e400e3eea9f5513e4652d5e1b1530427
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-10-27 19:24:59 -0600

    Dependencias y db context

---
+++ PracticaProgramada2_Grupo2/Data/MinombredeconexionDbContextcs.cs
@@ -0,0 +1,11 @@
+﻿using PracticaProgramada2_Grupo2.Controllers;
+using PracticaProgramada2_Grupo2.Models;
+
+
+namespace PracticaProgramada2_Grupo2.Data
+{
+    public class MinombredeconexionDbContextcs
+    {
+
+    }
+}
