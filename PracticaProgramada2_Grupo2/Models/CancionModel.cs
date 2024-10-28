87211ff45893f950aefa50e4702c5b8b905f34a4
Cristopher Rodríguez <145072878+EnekoRAP@users.noreply.github.com>
2024-10-28 10:17:43 -0600

    Models restantes

---
+++ PracticaProgramada2_Grupo2/Models/CancionModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PracticaProgramada2_Grupo2.Models
+{
+    public class CancionModel
+    {
+        public int Id_Cancion { get; set; }
+        public string Titulo { get; set; }
+        public string Artista { get; set; }
+    }
+}
---
+++ PracticaProgramada2_Grupo2/Models/PlaylistModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PracticaProgramada2_Grupo2.Models
+{
+    public class PlaylistModel
+    {
+        public int Id_Playlist { get; set; }
+        public string Nombre_Playlist { get; set; }
+        public string Nombre_Creador { get; set; }
+        public string Descripcion_Playlist { get; set; }
+        public string Genero_Playlist { get; set; }
+        public DateTime Fecha_Creacion { get; set; }
+    }
+}
